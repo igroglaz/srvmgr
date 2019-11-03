8d42454797f2b655045aed1fcd0054dded0e623b
serg-bloim <serg.bloim@gmail.com>
2019-11-03 03:13:54 -0500

    Fix for spell duration overflow

---
+++ spell_duration_fix.cpp
@@ -0,0 +1,89 @@
+void __declspec(naked) fix_spell_duration_overflow_0053973E()
+{ // 0053973E
+    __asm
+    {
+		cmp		eax, 0xFFFF
+		jle		short ret_point1
+		mov		eax, 0xFFFF
+ret_point1:
+		// restore overridden code begin
+		mov     edx, [ebp-0xC]
+		mov     [edx+10h], ax
+		// restore overridden code 
+		// jump back
+        mov     edx, 0x00539745
+        jmp     edx
+    }
+}
+// FUNCTIONS BELOW ARE DIFFERENT
+void __declspec(naked) fix_spell_duration_overflow_0053BFA1()
+{ // 0053BFA1
+    __asm
+    {
+		cmp		eax, 65000
+		jle		short ret_point2
+		mov		eax, 65000
+ret_point2:
+		// restore overridden code begin
+		mov     edx, [ebp-0x44]
+		mov     [edx+0x42], ax
+		// restore overridden code 
+		// jump back
+        mov     edx, 0x0053BFA8
+        jmp     edx
+    }
+}
+
+void __declspec(naked) fix_spell_duration_overflow_0053C0E1()
+{ // 0053C0E1
+    __asm
+    {
+		cmp		eax, 65000
+		jle		short ret_point3
+		mov		eax, 65000
+ret_point3:
+		// restore overridden code begin
+		mov     edx, [ebp-0x44]
+		mov     [edx+0x42], ax
+		// restore overridden code 
+		// jump back
+        mov     edx, 0x0053C0E8
+        jmp     edx
+    }
+}
+
+void __declspec(naked) fix_spell_duration_overflow_0053C21E()
+{ // 0053C21E
+    __asm
+    {
+		cmp		eax, 65000
+		jle		short ret_point4
+		mov		eax, 65000
+ret_point4:
+		// restore overridden code begin
+		mov     edx, [ebp-0x44]
+		mov     [edx+0x42], ax
+		// restore overridden code 
+		// jump back
+        mov     edx, 0x0053C225
+        jmp     edx
+    }
+}
+
+void __declspec(naked) fix_spell_duration_overflow_0053C52A()
+{ // 0053C52A
+    __asm
+    {
+		cmp		eax, 65000
+		jle		short ret_point5
+		mov		eax, 65000
+ret_point5:
+		// restore overridden code begin
+		mov     edx, [ebp-0x44]
+		mov     [edx+0x42], ax
+		// restore overridden code 
+		// jump back
+        mov     edx, 0x0053C531
+        jmp     edx
+    }
+}
