8eb0c670fcc8ce046c5cd61d1ae7cfe0d8bff3b9
serg-bloim <serg.bloim@gmail.com>
2019-11-11 22:09:46 -0500

    Revert "new parameters"
    There is a compilation error caused by this commit.
    Plus it is advised to follow the new schema of using ordinal numbers. Every developer should aquire a pool of numbers and using them.
    
    This reverts commit 0ae7de7b658411326752276caea2e36ebe9489d7.

--- srvmgr_new.cpp
+++ srvmgr_new.cpp
@@ -10,8 +10,8 @@
 #include "scanrange.h"
 #include "unit_info.h"
 #include "forbidden_items.h"
-#include "python_a2.h"
 #include "multiplayer_shop.h"
+#include "python.h"
 
 void ChangeWndTitle(const char* title)
 {
