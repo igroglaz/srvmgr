184f90eef95a92593ee83aee01cebd6cb6c191c3
ivan <cool.pepyaka@gmail.com>
2025-08-24 06:19:03 +0200

    Reapply "Skip potion reward in the inn at nightmare+"
    This reverts commit aae1ba7a76bb053badf10d6259eca69bc68f212d.
    
    Fix: `cmp eax, 0` compares 4-byte register, but `SkipPotionReward` sets
    only `al`, a 1-byte register. The function never touches the full `eax`,
    so its three leftmost bytes are left with garbage values from previous
    instructions, and they're unlikely 0.

--- quests.cpp
+++ quests.cpp
@@ -435,3 +435,28 @@ void __declspec(naked) inn_scrolls_reward() {
         jmp ebx
     }
 }
+
+bool SkipPotionReward() {
+    return Config::ServerID >= NIGHTMARE;
+}
+
+// Address: 005660c8
+void __declspec(naked) skip_potion_reward() {
+    __asm {
+        call SkipPotionReward
+
+        test al, al
+        jnz skip
+
+        // Original instruction.
+        mov edx, DWORD PTR [ebp+0x8]
+        mov eax, DWORD PTR [edx+0x38]
+
+        mov ebx, 0x005660ce
+        jmp ebx
+
+    skip:
+        mov ebx, 0x0056661c
+        jmp ebx
+    }
+}
