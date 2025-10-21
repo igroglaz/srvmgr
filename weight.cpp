918397fbf3a9d3e2be01305b929ecd66855cdebf
tangar <39904162+igroglaz@users.noreply.github.com>
2025-10-21 11:09:01 +0300

    Reaction affects weight only for circle witches

--- weight.cpp
+++ weight.cpp
@@ -1,6 +1,7 @@
 #include <cstdint>
 
 #include "a2types.h"
+#include "circle.h"
 
 // Address: 0052a29e.
 void __declspec(naked) replace_unit_carrying_body_with_reaction() {
@@ -10,11 +11,20 @@ void __declspec(naked) replace_unit_carrying_body_with_reaction() {
         mov [unit], eax
     }
 
-    uint32_t reaction;
-    reaction = unit->reaction;
+    uint32_t value;
+    
+    // Check if unit and player are valid, and if it's a circle witch
+    if (unit && unit->player && 
+        circle::Circle(unit) > 0 && !IsWarrior(unit) && IsFemale(unit)) {
+        // Use reaction for circle witches
+        value = unit->reaction;
+    } else {
+        // Vanilla way for everyone else
+        value = unit->body;
+    }
 
     __asm {
-        mov ecx, DWORD PTR[reaction]
+        mov ecx, DWORD PTR[value]
 
         mov edx, 0x0052a2a5
         jmp edx
@@ -29,11 +39,20 @@ void __declspec(naked) replace_human_carrying_body_with_reaction() {
         mov [unit], edx
     }
 
-    uint32_t reaction;
-    reaction = unit->reaction;
+    uint32_t value;
+    
+    // Check if unit and player are valid, and if it's a circle witch
+    if (unit && unit->player && 
+        circle::Circle(unit) > 0 && !IsWarrior(unit) && IsFemale(unit)) {
+        // Use reaction for circle witches
+        value = unit->reaction;
+    } else {
+        // Vanilla way for everyone else
+        value = unit->body;
+    }
 
     __asm {
-        mov eax, DWORD PTR[reaction]
+        mov eax, DWORD PTR[value]
 
         mov ecx, 0x00531a12
         jmp ecx
